48d2a7c9541455e524b6f2d2d99fb954996d0bd8
longjing_lanyan <2830068416@qq.com>
2026-04-30 18:23:01 +0800

    将wiki纳入安卓用户访问区域

--- src/main/java/com/genshin/gm/config/WebConfig.java
+++ src/main/java/com/genshin/gm/config/WebConfig.java
@@ -22,7 +22,8 @@ public class WebConfig implements WebMvcConfigurer {
                         "/data/**",         // 静态数据文件
                         "/download.html",   // 下载页自身
                         "/register.html",   // 玩家注册页（安卓玩家需要访问）
-                        "/html/register.html"
+                        "/html/register.html",
+                        "/wiki.html"    //wiki页面（安卓玩家需要访问）
                 );
     }
 
